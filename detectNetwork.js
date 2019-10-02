96b7416c79d6d95b601b3348c8a4b4a46296b1e8
Strider Wilson <Strider@Striders-MBP.T-mobile.com>
2019-10-01 22:31:22 -0700

    Adds 'China UnionPay', and 'Switch'

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,10 +13,19 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var china1 = parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3] + cardNumber[4] + cardNumber[5]);
+  var china2 = parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2]);
+  var china3 = parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3]);
+  var switch4 = parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3]);
+  var switch6 = parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3] + cardNumber[4] + cardNumber[5]);
   if ((cardNumber[0] + cardNumber[1] === '38') && (cardNumber.length === 14)) {
     return "Diner's Club";
   } else if ((cardNumber[0] + cardNumber[1] === '39') && (cardNumber.length === 14)) {
     return "Diner's Club";
+  } else if ((switch4 === 4903 || switch4 === 4905 || switch4 === 4911 || switch4 === 4936 || switch4 === 6333 || switch4 === 6759) && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+    return "Switch";
+  } else if ((switch6 === 564182 || switch6 === 633110) && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+    return "Switch";
   } else if ((cardNumber[0] + cardNumber[1] === '34') && (cardNumber.length === 15)) {
     return "American Express";
   } else if ((cardNumber[0] + cardNumber[1] === '37') && (cardNumber.length === 15)) {
@@ -47,6 +56,12 @@ var detectNetwork = function(cardNumber) {
     return 'Maestro'
   } else if ((cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3] === '6304') && (cardNumber.length >= 12 && cardNumber.length <= 19)) {
     return 'Maestro'
+  } else if ((china1 >= 622126 && china1 <= 622925) && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+    return 'China UnionPay'
+  } else if ((china2 >= 624 && china2 <= 626) && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+    return 'China UnionPay'
+  } else if ((china3 >= 6282 && china3 <= 6288) && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+    return 'China UnionPay'
   }
 };
 
