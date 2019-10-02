f47c08976c84928b41ba3340a1fa279bc340ac2e
Strider Wilson <Strider@Striders-MBP.T-mobile.com>
2019-10-01 20:24:00 -0700

    Adds 'Discover' card identifier

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -13,14 +13,32 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  if ((cardNumber[0] + cardNumber[1] === '38' || '39') && (cardNumber.length === 14)) {
+  if ((cardNumber[0] + cardNumber[1] === '38') && (cardNumber.length === 14)) {
     return "Diner's Club";
-  } else if ((cardNumber[0] + cardNumber[1] === '34' || '37') && (cardNumber.length === 15)) {
+  } else if ((cardNumber[0] + cardNumber[1] === '39') && (cardNumber.length === 14)) {
+    return "Diner's Club";
+  } else if ((cardNumber[0] + cardNumber[1] === '34') && (cardNumber.length === 15)) {
     return "American Express";
-  } else if ((cardNumber[0] === '4') && (cardNumber.length === 13 || 16 || 19)) {
+  } else if ((cardNumber[0] + cardNumber[1] === '37') && (cardNumber.length === 15)) {
+    return "American Express";
+  } else if ((cardNumber[0] === '4') && (cardNumber.length === 13 || cardNumber.length === 16 ||   cardNumber.length === 19)) {
     return "Visa"
-  } else if ((cardNumber[0] + cardNumber[1] === '51' || '52' || '53' || '54') && (cardNumber.length === 16)) {
+  } else if ((cardNumber[0] + cardNumber[1] === '51') && (cardNumber.length === 16)) {
     return 'MasterCard'
+  } else if ((cardNumber[0] + cardNumber[1] === '52') && (cardNumber.length === 16)) {
+    return 'MasterCard'
+  } else if ((cardNumber[0] + cardNumber[1] === '53') && (cardNumber.length === 16)) {
+    return 'MasterCard'
+  } else if ((cardNumber[0] + cardNumber[1] === '54') && (cardNumber.length === 16)) {
+    return 'MasterCard'
+  } else if ((cardNumber[0] + cardNumber[1] === '55') && (cardNumber.length === 16)) {
+    return 'MasterCard'
+  } else if ((cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3] === '6011') && (cardNumber.length === 16 || cardNumber.length === 19)) {
+    return 'Discover'
+  } else if ((644 <= parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2]) && parseInt(cardNumber[0] + cardNumber[1] + cardNumber[2]) <= 649) && (cardNumber.length === 16 || cardNumber.length === 19)) {
+    return 'Discover'
+  } else if ((cardNumber[0] + cardNumber[1] === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
+    return 'Discover'
   }
 };
 
